07930755d2d92f49e6aba8502b76445ea6e39d72
agvan <agvandyke25@gmail.com>
2022-09-01 16:44:36 -0500

    added the WebpackPwaManifest plugin to the webpack config

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -5,7 +5,6 @@ const { InjectManifest } = require("workbox-webpack-plugin");
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 
-
 module.exports = () => {
   return {
     mode: "development",
@@ -19,11 +18,30 @@ module.exports = () => {
     },
     plugins: [
       new HtmlWebpackPlugin({
-        template: './index.html',
-        title: 'Webpack Plugin'
-      })
-        
-      
+        template: "./index.html",
+        title: "Webpack Plugin",
+      }),
+      new WebpackPwaManifest({
+        name: "PWA-Text-Editor Application",
+        short_name: "Text Editor",
+        description: "Edit that text!",
+        background_color: "",
+        theme_color: "",
+        start_url: "./",
+        icons: [
+          {
+            src: path.resolve("src/images/icon-manifest.png"),
+            sizes: [72, 96, 128, 144, 152, 192, 384, 512],
+            destination: path.join("assets", "icons"),
+          },
+          {
+            src: path.resolve("src/images/icon-manifest.png"),
+            size: "1024x1024",
+            destination: path.join("assets", "icons"),
+            purpose: "maskable",
+          },
+        ],
+      }),
     ],
 
     module: {
