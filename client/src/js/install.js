bbcc46e7921ddaa151ba388dd3f9543816117674
agvan <agvandyke25@gmail.com>
2022-09-02 11:47:02 -0500

    added click event handler for butInstall element, added one comment, & updated two comments

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,14 +1,27 @@
-const butInstall = document.getElementById('buttonInstall');
+const butInstall = document.getElementById("buttonInstall");
 
 // Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {
-    window.deferredPrompt = event;
-    butInstall.style.visibility = 'visible';
+// Event handler to the `beforeinstallprompt` event
+window.addEventListener("beforeinstallprompt", (event) => {
+  window.deferredPrompt = event;
+  butInstall.style.visibility = "visible";
 });
 
-// TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+// Click event handler on the `butInstall` element
+butInstall.addEventListener("click", async () => {
+  const promptEvent = window.deferredPrompt;
+
+  if (!promptEvent) {
+    return;
+  }
+
+  promptEvent.prompt();
+  window.deferredPrompt = null;
+
+  // prevents the console log from reading an error saying that "e.split" isn't a function
+  butInstall.classList.toggle("hidden", true);
+  butInstall.textContent = "Installed!";
+});
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener("appinstalled", (event) => {});
